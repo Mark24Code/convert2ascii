c62fc68e86390d6396761856edc477debcd6c7ec
Mark24 <mark.zhangyoung@gmail.com>
2025-01-12 16:22:30 +0800

    feat: video2ascii exe & test

---
+++ lib/convert2ascii.rb
@@ -0,0 +1,3 @@
+require_relative './convert2ascii/version.rb'
+require_relative './convert2ascii/image2ascii.rb'
+require_relative './convert2ascii/video2ascii.rb'
---
+++ test/test_03_video2ascii_play.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require_relative '../lib/convert2ascii/video2ascii'
+
+class Video2AsciiPlay < Minitest::Test
+  def setup
+    @width = 20
+    @fake_uri = "./test/assets/fireworks.mp4"
+    @generator = Convert2Ascii::Video2Ascii.new(uri:@fake_uri, width: @width)
+  end
+
+  def test_video_play
+    @generator.generate
+    assert_equal true , @generator.play
+  end
+end
