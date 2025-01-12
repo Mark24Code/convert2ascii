df572d39764066eb139cb264dfb3c08561941eb8
Mark24 <mark.zhangyoung@gmail.com>
2025-01-12 16:21:49 +0800

    feat: video2ascii test

---
+++ test/test_02_video2ascii.rb
@@ -0,0 +1,23 @@
+require "minitest/autorun"
+require_relative '../lib/convert2ascii/video2ascii'
+
+class TestDefaultParams < Minitest::Test
+  def setup
+    @width = 20
+    @fake_uri = "./test/assets/fireworks.mp4"
+    @generator = Convert2Ascii::Video2Ascii.new(uri: @fake_uri, width: @width)
+  end
+
+  def test_initialize_params_width
+    assert_equal @width, @generator.width
+  end
+
+  def test_initialize_params_duration
+    assert_equal Convert2Ascii::Video2Ascii::DEFAULT_STEP_DURATION, @generator.step_duration
+  end
+
+  def test_generate_params
+    test_width = 200
+    assert_equal test_width , @generator.generate(width: test_width).width
+  end
+end
